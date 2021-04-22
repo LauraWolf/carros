8605784f1f4f7baca8f3acc1c1eb8a0b75fc951f
LauraWolf <wolflaura03@gmail.com>
2021-04-22 11:27:08 -0300

    Quarto Commit

---
+++ src/sample/model/Veiculo.java
@@ -0,0 +1,58 @@
+package sample.model;
+
+public abstract class Veiculo {
+    /**
+     * Atributos
+     */
+    private String nome;
+    private Marca marca;
+    private String desc;
+
+    /**
+     * Construtor da classe
+     * @param nome
+     */
+    public Veiculo(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    public void setMarca(String nome, String descricao) {
+        Marca marca = new Marca();
+        marca.setNome(nome);
+        marca.setDesc(descricao);
+        this.marca = marca;
+    }
+    public String getDesc() {
+        return desc;
+    }
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    /**
+     * ToString da classe
+     * @return string do objeto
+     */
+    @Override
+    public String toString() {
+        return nome + ", " + marca
+                + ((desc == null) ? "" : desc + "\n");
+    }
+
+}
